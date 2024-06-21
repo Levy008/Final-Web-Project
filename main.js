0f33804fcd7af00e80c60727b9689126e4d636dd
Tanaka <t.levie8@gmail.com>
2024-06-21 15:01:52 +0300

    modified javascript file for responsive mobile layout hamburger menu and search bar dropdowns

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// aside links section
+
 let active = document.querySelector('.activeSelection');
 let active1 = document.querySelector('.activeSelection1');
 let aside = document.querySelector('.asideList');
@@ -18,3 +20,28 @@ active1.addEventListener('click', () => {
     icon2.classList.toggle('hideSidelist');
     icon3.classList.toggle('hideSidelist');
 });
+
+
+// mobile hamburger menu open and close
+
+let menuOpen = document.querySelector('.menuOpen');
+let menuClose = document.querySelector('.menuClose');
+let navigate = document.querySelector('.navigator');
+let mobileSearch = document.querySelector('.mobileSearch');
+let searchBar = document.querySelector('.searchBar')
+let bxClose = document.querySelector('.bx-x');
+
+function openCloseMenu() {
+    menuOpen.classList.toggle('hide');
+    menuClose.classList.toggle('hide');
+    navigate.classList.toggle('hide');
+    mobileSearch.classList.toggle('hide');
+}
+
+function searchBox() {
+    searchBar.classList.toggle('hide');
+    mobileSearch.classList.toggle('hide');
+    menuOpen.classList.toggle('hide');
+    menuClose.classList.toggle('hide');
+    bxClose.classList.toggle('bxLeft');
+}
